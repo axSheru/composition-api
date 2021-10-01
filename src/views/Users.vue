5d515ce2094d3a77f98f74711370db98632d5e9e
Alex Paredes <alexparedesr95@gmail.com>
2021-09-30 22:12:12 -0500

    FEAT: Se crea la vista Users.

---
+++ src/views/Users.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>Espere por favor...</h2>
+  <h2>Usuarios</h2>
+  <h5>Error en la carga...</h5>
+
+  <div>
+      <ul>
+          <li>
+              <h4>Nombre de la persona</h4>
+              <h6>email@gmail.com</h6>
+          </li>
+      </ul>
+  </div>
+
+  <button>Atrás</button>
+  <button>Siguiente</button>
+  <span>Página: 5</span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
