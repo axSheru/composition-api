19ac2f785ff48c3e8ee4500d10872698edfbc096
Alex Paredes <alexparedesr95@gmail.com>
2021-09-30 22:29:57 -0500

    FEAT: Se realiza petición para obtener los usuarios.

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -18,7 +18,41 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+import axios from 'axios'
+
 export default {
+
+    setup() {
+
+        const users = ref([])
+        const isLoading = ref(true)
+        const currentPage = ref(1)
+        const errorMessage = ref(1)
+
+        const getUsers = async( page = 1 ) => {
+
+            if( page <= 0 ) page = 1
+
+            isLoading.value = true
+
+            const { data } = await axios.get('https://reqres.in/api/users', {
+                params: { page }
+            })
+
+            if( data.data.length > 0 ) {
+                users.value = data.data
+                currentPage.value = page
+            } else if( currentPage.value > 0 ) {
+                errorMessage.value = 'No hay mas usuarios.'
+            }
+
+        }
+
+        getUsers()
+
+
+    }
 
 }
 </script>
