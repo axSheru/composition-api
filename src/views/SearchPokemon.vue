a2db2675e14697a3568addbce46f9dfd4581ce61
Alex Paredes <alexparedesr95@gmail.com>
2021-10-01 22:52:08 -0500

    FEAT: Se crea la base de la vista SearchPokemon.

---
+++ src/views/SearchPokemon.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Buscar Pokémon: </h1>
+  <form>
+      <input
+        type="number"
+        placeholder="Número del Pokémon"
+        autofocus
+      >
+  </form>
+  <br>
+  <span>Presione enter para buscar</span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
