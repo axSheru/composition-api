28b77c69b1ac80cdddbcb85b72a3764a4ad15c98
Alex Paredes <alexparedesr95@gmail.com>
2021-10-01 22:52:29 -0500

    FEAT: Se crea la ruta para la vista SearchPokemon.

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/users',
     name: 'users',
     component: () => import(/* webpackChunkName: "users" */ '../views/Users.vue')
+  },
+  {
+    path: '/pokemon-search',
+    name: 'pokemon-search',
+    component: () => import(/* webpackChunkName: "pokemon-search" */ '../views/SearchPokemon.vue')
   }
 ]
 
