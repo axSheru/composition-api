0c511ef8d0f3a56562e0af29571cb66edd1b26a8
Alex Paredes <alexparedesr95@gmail.com>
2021-10-02 22:15:07 -0500

    FEAT: Se crea composable donde se obtiene la información del Pokémon.

---
+++ src/composables/usePokemon.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { ref } from 'vue'
+
+const usePokemon = ( pokemonId = '1' ) => {
+
+    const pokemon = ref()
+    const isLoading = ref(false)
+    const errorMessage = ref()
+
+    const searchPokemon = async() => {
+
+        isLoading.value = true
+        pokemon.value = null
+
+        try {
+
+            const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${ pokemonId }`)
+            pokemon.value = data
+            errorMessage.value = ''
+
+            console.log(pokemon)
+            
+        } catch (error) {
+            errorMessage.value = 'No se pudo cargar ese Pokémon.'
+        }
+
+        isLoading.value = false
+
+    }
+
+    searchPokemon()
+
+    return {
+        errorMessage,
+        isLoading,
+        pokemon,
+    }
+
+}
+
+export default usePokemon
