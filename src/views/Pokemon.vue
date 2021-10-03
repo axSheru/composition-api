32e139ef1befea04581fc5230d24dbaf0187ec9a
Alex Paredes <alexparedesr95@gmail.com>
2021-10-02 22:15:31 -0500

    FEAT: Se importa el composable usePokemon y se implementa.

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,18 +1,34 @@
 <template>
-  <h1>Pokémon View</h1>
+  <h1 v-if="!pokemon && !errorMessage">Buscando...</h1>
+  <h1 v-else-if="errorMessage">{{errorMessage}}</h1>
+
+  <template v-else>
+    <h3>{{ pokemon.name }}</h3>
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    <br>
+    <router-link :to="{ name: 'pokemon-search' }">Regresar</router-link>
+  </template>
+
 </template>
 
 <script>
-import { ref } from 'vue'
+
 import { useRoute } from 'vue-router'
+import usePokemon from '@/composables/usePokemon'
 
 export default {
 
     setup() {
+      
+      const route = useRoute()
+      
+      const { errorMessage, isLoading, pokemon } = usePokemon( route.params.id )
 
-        const route = useRoute()
-
-        
+      return {
+        errorMessage,
+        isLoading,
+        pokemon
+      }
 
     }
 
