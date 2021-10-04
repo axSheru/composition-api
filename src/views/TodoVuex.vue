365ac523fcdcab8562723b6537b69a8fb9eb9ed9
Alex Paredes <alexparedesr95@gmail.com>
2021-10-03 23:35:02 -0500

    FEAT: Se crea una propiedad computada para user un getter y se muestra la información de cuántas tareas pendientes hay en la vista..

--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -1,9 +1,24 @@
 <template>
   <h1>Lista de tareas de Thanos.</h1>
+  <!-- <h4>Pendientes: {{ $store.state.todos.filter( t => !t.completed ).length }}</h4> -->
+  <h4>Pendientes:  {{ pending.length }} </h4>
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
 export default {
+
+  setup() {
+
+    const store = useStore()
+
+    return {
+      pending: computed( () => store.getters['pendingTodos'] )
+    }
+
+  }
 
 }
 </script>
