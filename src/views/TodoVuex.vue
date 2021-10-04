a9b0234eb27c9e11acfb4234048728f3681c0c8d
Alex Paredes <alexparedesr95@gmail.com>
2021-10-03 23:48:41 -0500

    FEAT: Se importan los getters en propiedades computadas y se muestran en la vista.

--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -2,6 +2,12 @@
   <h1>Lista de tareas de Thanos.</h1>
   <!-- <h4>Pendientes: {{ $store.state.todos.filter( t => !t.completed ).length }}</h4> -->
   <h4>Pendientes:  {{ pending.length }} </h4>
+
+  <hr>
+
+  <h4>Todos:  {{ all.length }} </h4>
+  <h4>Completados:  {{ completed.length }} </h4>
+
 </template>
 
 <script>
@@ -14,8 +20,14 @@ export default {
 
     const store = useStore()
 
+    //NOTE
+    /* Se tiende a crear la propiedad computada directo en el return si solamente se utilizará en el template.
+    Se genera en el setup si la necesitamos manipular para otra función, atributo o propiedad. */
+
     return {
-      pending: computed( () => store.getters['pendingTodos'] )
+      pending: computed( () => store.getters['pendingTodos'] ),
+      all: computed( () => store.getters['allTodos'] ),
+      completed: computed( () => store.getters['completedTodos'] ),
     }
 
   }
