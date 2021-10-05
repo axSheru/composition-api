2a15957fd3799c08e38899bddb72f036c66d2588
Alex Paredes <alexparedesr95@gmail.com>
2021-10-04 23:04:44 -0500

    FEAT: Se crea vista CustomSlots.

---
+++ src/views/CustomSlots.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Aprendiendo Slots</h1>
+  <button>Abrir modal</button>
+
+  <Modal />
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+
+export default {
+
+    components: { Modal },
+    setup() {
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
